3e2c5d11ecb436711cb72289f59f2c18a18d8d2d
simbo1905 <simbo1905@60hertz.com>
2018-03-13 23:04:54 +0000

    allow port override to WEBAPP_SERVICE_PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.WEBAPP_SERVICE_PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -44,7 +45,7 @@ i18n
         // use next.js
         server.get('*', (req, res) => handle(req, res))
 
-        server.listen(3000, (err) => {
+        server.listen(port, (err) => {
           if (err) throw err
           console.log('> Ready on http://localhost:3000')
         })
