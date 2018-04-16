ab92a02546cadc5f02a4e9f4e048d4a0df550f5f
Sergey Sevrugin <sevrugin@gmail.com>
2018-04-16 13:35:24 +0300

    + Add Organizations list to the header + Add Groups icons

--- app/containers/Groups/index.js
+++ app/containers/Groups/index.js
@@ -5,20 +5,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {initialize} from 'redux-form';
+import { initialize } from 'redux-form';
 import { Helmet } from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import withGuard from 'utils/withGuard';
 import GroupForm from './GroupForm';
 import InfoForm from './InfoForm';
-import {
-  saveGroupRequestAPI,
-} from '../../api/requests/groups';
+import { saveGroupRequestAPI } from '../../api/requests/groups';
 
 import {
   organizationsListRequested,
-  organizationsCurrentChangeRequested
+  organizationsCurrentChangeRequested,
 } from '../Organizations/redux/actions';
 
 import {
@@ -27,8 +25,7 @@ import {
   groupRemoveRequested,
 } from './redux/actions';
 
-import Avatar2 from 'static/images/avatars/2.jpg';
-import AvatarM from 'static/images/avatars/m.png';
+import AvatarBlank from 'static/images/avatars/avatar-blank.svg';
 
 import {
   Row,
@@ -90,16 +87,16 @@ export class Groups extends React.Component {
     this.infoForm.wrappedInstance.show();
   }
 
-  onSubmit = (values) => {
-    return saveGroupRequestAPI(values).then(() => {
-      this.hide();
-      this.props.groupsListRequested(this.props.organizations.currentId);
-    });
-  };
+  onSubmit = (values) => saveGroupRequestAPI(values).then(() => {
+    this.hide();
+    this.props.groupsListRequested(this.props.organizations.currentId);
+  });
 
   render() {
     const {
+      organizations,
       groups,
+      onOrganizationChanged,
     } = this.props;
 
     return (
@@ -115,8 +112,8 @@ export class Groups extends React.Component {
             <Row>
               <Col sm="1" className="align-self-center">Organization</Col>
               <Col sm="9">
-                <Input className="rounded" type="select" name="select" id="organization" onChange={this.props.onOrganizationChanged}>
-                  {this.props.organizations.list.map((organization) => <option key={organization.id} value={organization.id}>{organization.name}</option>)}
+                <Input className="rounded" type="select" name="select" id="organization" onChange={onOrganizationChanged}>
+                  {organizations.list.map((organization) => <option key={organization.id} value={organization.id}>{organization.name}</option>)}
                 </Input>
               </Col>
             </Row>
@@ -143,19 +140,19 @@ export class Groups extends React.Component {
           </CardHeader>
           <CardBody>
             <Row>
-              {this.props.groups.list.map((group, index) =>
-                <Col key={group.id} xs="12" sm="6" md="6" lg="4" xl="3" xxl="1" className="mb-3">
-                  <div className="message" onClick={() => {this.showGroupInfo(index)}} style={{cursor: 'pointer'}}>
+              {groups.list.map((group, index) =>
+                (<Col key={group.id} xs="12" sm="6" md="6" lg="4" xl="3" xxl="1" className="mb-3">
+                  <div className="message" onClick={() => { this.showGroupInfo(index); }} style={{ cursor: 'pointer' }}>
                     <div className="py-1 mr-3 float-left">
                       <div className="avatar">
-                        <img src={AvatarM} className="img-avatar" alt="Marketing" />
+                        <div className="avatar-letter">{group.name[0].toLowerCase()}</div>
                       </div>
                     </div>
                     <div>
                       <small className="text-muted float-right mt-1">
                         {group.users.filter((user) => user.status === 'active').length} user(s)<br />
-                        {/*<Button onClick={() => { this.removeGroup(group); }}>[ remove ]</Button>*/}
-                        {/*<Button onClick={() => { this.editGroup(group); }}>[ edit ]</Button>*/}
+                        {/* <Button onClick={() => { this.removeGroup(group); }}>[ remove ]</Button> */}
+                        {/* <Button onClick={() => { this.editGroup(group); }}>[ edit ]</Button> */}
                       </small>
                     </div>
                     <div className="text-truncate font-weight-bold" title={group.description}>
@@ -163,14 +160,12 @@ export class Groups extends React.Component {
                     </div>
                     <div className="avatars-stack mt-2">
                       {group.users.filter((user) => user.status === 'active').map((user) =>
-                      <div key={user.id} className="avatar avatar-s">
-                        <img src={Avatar2} className="img-avatar" title={user.name + ' [' + user.email + ']'} />
-                      </div>
-                      )}
+                        (<div key={user.id} className="avatar avatar-s">
+                          <img src={AvatarBlank} className="img-avatar" title={`${user.name} [${user.email}]`} />
+                         </div>))}
                     </div>
                   </div>
-                </Col>
-              )}
+                 </Col>))}
             </Row>
           </CardBody>
         </Card>
@@ -209,16 +204,14 @@ export function mapDispatchToProps(dispatch) {
   };
 }
 
-const mapStateToProps = (state) => {
-  return {
-    groups: state.groups,
-    organizations: state.organizations,
-  };
-};
+const mapStateToProps = (state) => ({
+  groups: state.groups,
+  organizations: state.organizations,
+});
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
-const withGroupSaga = injectSaga({ key: 'groups', saga: groupSaga});
-const withOrganizationSaga = injectSaga({ key: 'organizations', saga: organizationSaga});
+const withGroupSaga = injectSaga({ key: 'groups', saga: groupSaga });
+const withOrganizationSaga = injectSaga({ key: 'organizations', saga: organizationSaga });
 
 export default compose(
   withGroupSaga,
--- app/layouts/App/components/Header/index.js
+++ app/layouts/App/components/Header/index.js
@@ -1,14 +1,49 @@
 // eslint-disable
 import React, { Component } from 'react';
 import {
-  Nav,
-  NavItem,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
   NavbarToggler,
   NavbarBrand,
-  NavLink,
 } from 'reactstrap';
 
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import {
+  organizationsListRequested,
+  organizationsCurrentChangeRequested
+} from '../../../../../app/containers/Organizations/redux/actions';
+
+import {
+  groupsListRequested,
+} from '../../../../../app/containers/Groups/redux/actions';
+
+
+import injectSaga from 'utils/injectSaga';
+import organizationSaga from '../../../../../app/containers/Organizations/redux/saga';
+
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    this.props.organizationsListRequested();
+  }
+
+  toggleDropDown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
 
   sidebarToggle(e) {
     e.preventDefault();
@@ -31,18 +66,62 @@ class Header extends Component {
   }
 
   render() {
+    if ( this.props.organizations.list.length === 0) return '';
     return (
       <header className="app-header navbar">
         <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>
           <span className="navbar-toggler-icon"></span>
         </NavbarToggler>
         <NavbarBrand href="#"></NavbarBrand>
-        <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
+
+        <Dropdown nav isOpen={this.state.dropdownOpen} toggle={() => { this.toggleDropDown(); }} className="float-right list-style-none">
+          <DropdownToggle nav caret>
+            <i className="icon-cup" /> {this.props.organizations.list[0].name }
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Company</DropdownItem>
+            {this.props.organizations.list.map((organization) =>
+              <DropdownItem key={organization.id} onClick={() => this.props.onOrganizationChanged(organization.id)}>{organization.name}</DropdownItem>
+            )}
+            <DropdownItem disabled>Add company...</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        <NavbarToggler className="float-right mr-4" onClick={this.asideToggle}>
           <span className="navbar-toggler-icon"></span>
         </NavbarToggler>
+
+
       </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  organizations: PropTypes.object,
+  organizationsListRequested: PropTypes.func,
+  onOrganizationChanged: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    organizationsListRequested: () => dispatch(organizationsListRequested()),
+    onOrganizationChanged: (id) => {
+      dispatch(organizationsCurrentChangeRequested(id));
+      dispatch(groupsListRequested(id));
+    },
+  };
+}
+
+const mapStateToProps = (state) => ({
+  groups: state.groups,
+  organizations: state.organizations,
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+const withOrganizationSaga = injectSaga({ key: 'organizations', saga: organizationSaga });
+
+export default compose(
+  withOrganizationSaga,
+  withConnect,
+)(Header);
