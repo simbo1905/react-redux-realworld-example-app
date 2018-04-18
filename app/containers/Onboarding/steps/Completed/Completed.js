d67084bd859afac106bf4fff2ec77739103028aa
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-04-18 21:02:18 +0200

    Added a button to completed. The flow can now be finished but it still needs a lot of work.

--- app/containers/Onboarding/steps/Completed/Completed.js
+++ app/containers/Onboarding/steps/Completed/Completed.js
@@ -3,22 +3,39 @@
  */
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 import FluidHeader from 'components/landingpage/FluidHeader';
 import FluidButton from 'components/landingpage/FluidButton';
+import FluidFormWrap from 'components/landingpage/FluidFormWrap';
 import messages from './messages';
 
 class Completed extends Component { /* eslint-disable-line */
+  static contextTypes = {
+    router: PropTypes.object.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.goToDashboard = this.goToDashboard.bind(this);
+  }
+
+  goToDashboard() {
+    this.context.router.history.replace('/dashboard');
+  }
+
   render() {
     return (
       <div className="fluid-content">
         <FluidHeader
           title={<FormattedMessage {...messages.title} />}
-          sub={<FormattedMessage {...messages.sub} />}
+          sub={<FormattedHTMLMessage {...messages.sub} />}
         />
-        <FluidButton hasArrow>
-          <FormattedMessage {...messages.buttonLabel} />
-        </FluidButton>
+        <FluidFormWrap>
+          <FluidButton onClick={this.goToDashboard} hasArrow>
+            <FormattedMessage {...messages.buttonLabel} />
+          </FluidButton>
+        </FluidFormWrap>
       </div>
     );
   }
