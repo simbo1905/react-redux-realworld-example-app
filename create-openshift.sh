4ec5a00cd43508ab77e0f13ae602de98606f4050
simbo1905 <simbo1905@60hertz.com>
2018-04-21 21:24:03 +0100

    will use local backend API

--- create-openshift.sh
+++ create-openshift.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SOURCE_REPOSITORY_REF=$(git status | awk 'NR==1{print $NF}')
 if [[ -n "$SOURCE_REPOSITORY_REF" ]]; then
-  NAMESPACE=$(oc project --short)
+  PROJECT=$(oc project --short)
   echo git branch is $SOURCE_REPOSITORY_REF
   oc process -f openshift-template.json  \
-    -p NAME=webapp \
-    -p NAMESPACE=$NAMESPACE \
+    -p NAME=$NAME \
+    -p NAMESPACE=$PROJECT \
     -p SOURCE_REPOSITORY_URL=https://github.com/simbo1905/react-redux-realworld-example-app.git \
     -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
   | oc create -f -	
