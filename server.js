34355d8e32366538471b9a052d5e2d79a17a144b
simbo1905 <simbo1905@60hertz.com>
2018-05-04 19:57:07 +0100

    trying headers

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ app.use(express.static(path.join(__dirname, 'build')));
 const WINDOW_ENV = "window.env={'API_ROOT':'"+process.env.API_ROOT+"'}\n";
 
 app.get('/env.js', function (req, res) {
+  res.setHeader("X-Massey", "whatever");
   res.setHeader("Cache-Control", "public, max-age=300");
   res.set('Content-Type', 'application/javascript');
   res.send(WINDOW_ENV);
 });
 
 app.get('/*', function (req, res) {
+  res.setHeader("X-Massey", "whatever");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
